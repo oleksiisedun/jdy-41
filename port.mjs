0d3ce3d0fa1694bd75220005e5baaf9d32b42d95
Oleksii Sedun <oleksiisedun@gmail.com>
2023-08-01 15:46:59 +0300

    Move regex to function default parameter, update const names

--- port.mjs
+++ port.mjs
@@ -1,18 +1,14 @@
 import { SerialPort } from 'serialport';
 
-const BAUD_RATE = 9600;
-const PATH = await getPortPath();
+const baudRate = 9600;
+const path = await getPortPath();
 
-async function getPortPath() {
+async function getPortPath(regex = /tty.*usb/i) {
   const ports = await SerialPort.list();
-  const port = ports.find(({ path }) => /tty.*usb/i.test(path));
+  const port = ports.find(({ path }) => regex.test(path));
   if (!port) throw new Error('Serial port not found');
 
   return port.path;
 }
 
-export default new SerialPort({
-  path: PATH,
-  baudRate: BAUD_RATE,
-  autoOpen: false
-});
+export default new SerialPort({ baudRate, path, autoOpen: false });
